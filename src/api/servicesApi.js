e80cd68177aa85dfe036cc4c5b125bcc19e39c17
Roman Lytvynenko <litvinenko95@gmail.com>
2018-06-20 16:37:19 +0300

    added api implementations

---
+++ src/api/servicesApi.js
@@ -0,0 +1,26 @@
+/**
+ * Services API.
+ *
+ * @since [*next-version*]
+ *
+ * @param {string} resourceUrl URL of services resource.
+ * @param {HttpClient} httpClient Promise-based http client.
+ *
+ * @return {object} Map of pure API functions for services.
+ */
+export default function (resourceUrl, httpClient) {
+  return {
+    /**
+     * Fetch list of services.
+     *
+     * @since [*next-version*]
+     *
+     * @param {object} params Params for retrieving services
+     *
+     * @return {Promise<any>} Fetching services promise.
+     */
+    fetch (params = {}) {
+      return httpClient.get(resourceUrl, { params })
+    }
+  }
+}
