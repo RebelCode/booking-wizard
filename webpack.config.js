8b8b6949bfb705cf6961cd8d730ae1bc6c068ab4
Roman Lytvynenko <litvinenko95@gmail.com>
2018-06-21 13:59:44 +0300

    updated and installed JS deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+var debug = process.env.NODE_ENV !== 'production'
+var webpack = require('webpack')
 
 let config = {
-    context: __dirname,
+  context: __dirname,
 
-    devtool: debug ? "inline-sourcemap" : false,
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-          sourcemap: false
-        }),
-    ],
-    module: {
-        rules: [
-            { test: /\.js$/, loader: "babel-loader" }
-        ]
-    },
-    entry: "./src/app.js",
-    output: {
-        path: __dirname + "/dist/js",
-        filename: "app.min.js",
-        libraryTarget: 'umd'
-    }
-};
+  devtool: debug ? 'inline-sourcemap' : false,
+  plugins: debug ? [
+    new webpack.optimize.HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: true
+    }),
+  ] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      sourcemap: false
+    }),
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, loader: 'babel-loader'}
+    ]
+  },
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'app.min.js',
+    libraryTarget: 'umd'
+  }
+}
 
-module.exports = config;
+module.exports = config
